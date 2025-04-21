77a918f73bc37eb11fbdd404d1c949f87a207dba
Donika Stoyanova <donikas@gmail.com>
2025-04-21 22:13:11 +0300

    Commit

---
+++ src/main/java/bg/tu_varna/sit/taskmanager16042025/component/CustomMapper.java
@@ -0,0 +1,17 @@
+package bg.tu_varna.sit.taskmanager16042025.component;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.config.Configuration;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomMapper extends ModelMapper {
+
+    public CustomMapper() {
+        this.getConfiguration()
+                .setSkipNullEnabled(true)
+                .setFieldMatchingEnabled(true)
+                .setFieldAccessLevel(Configuration.AccessLevel.PRIVATE)
+                .setAmbiguityIgnored(true);
+    }
+}
